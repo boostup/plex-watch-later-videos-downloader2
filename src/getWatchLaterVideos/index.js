5293bf8e30e91452ec24b53a9f7097a412f1f864
Fred B <boostup@gmail.com>
2018-04-01 17:10:38 +0200

    modularisation of getWatchLaterVideos function

---
+++ src/getWatchLaterVideos/index.js
@@ -0,0 +1,51 @@
+const fs   = require('fs')
+const path = require('path')
+const plex = require('@boostup/plex-api')
+const ytdl = require('ytdl-core')
+const sanitize = require('sanitize-filename')
+const deleteFile = require('../utils/deleteFile')
+const writeFile = require('../utils/writeStrToFile')
+
+const getWatchLaterVideos = () => {
+	plex.getWatchLaterVideos()
+		.then(res => extractVideoUrls(res.MediaContainer.Video))
+		.then(logUrls)
+		.then(urls => urls.map(downloadVideo))
+		.catch((error) => {
+			console.error("The following error was caught:\n");
+            throw error;
+        })
+}
+
+module.exports = getWatchLaterVideos;
+
+/************************************** */
+
+const extractVideoUrls = videos => {
+	let videoUrls = new Array();
+	videos.map(vid=>
+		videoUrls.push(vid.url)
+	);
+	return videoUrls;
+}
+
+const logUrls = async urlsArr => {
+	const logFilePath = "urls.txt";
+	await deleteFile(logFilePath);
+	const urlsStr = urlsArr.join("\n");
+	console.log(`Videos to download:\n${urlsStr}`);
+	writeFile(logFilePath, urlsStr);
+	return urlsArr;
+}
+
+const downloadVideo = videoUrl => {
+	ytdl.getInfo(videoUrl).then(vidInfo => {
+		const formats = vidInfo.formats.filter(format => {
+			return format.container === "mp4" && format.quality === "medium"
+		})
+		const video = ytdl(videoUrl, {format: formats[0]});
+		video.pipe(fs.createWriteStream(path.resolve('./Documentaries', `${sanitize(vidInfo.title)}.mp4`)));
+	}, err => {
+	  console.error("this is now handled::::::::::>" + err)
+	})	
+}
