40f651a733403d9d2146b239d6817f9ebff17eec
Frédéric Beauvois <boostup@funcarousel-1.home>
2018-04-01 20:42:53 +0200

    rewrite of module

--- src/getWatchLaterVideos/index.js
+++ src/getWatchLaterVideos/index.js
@@ -2,49 +2,81 @@ const fs   = require('fs')
 const path = require('path')
 const plex = require('@boostup/plex-api')
 const ytdl = require('ytdl-core')
+const beautify = require('js-beautify').js_beautify
 const sanitize = require('sanitize-filename')
 const deleteFile = require('../utils/fs/deleteFile')
 const writeFile = require('../utils/fs/writeStrToFile')
 
-const getWatchLaterVideos = () => {
-	plex.getWatchLaterVideos()
-		.then(res => extractVideoUrls(res.MediaContainer.Video))
+const getWatchLaterVideos = async () => {
+
+	try {
+		console.log("=====> getWatchLaterVideos procedure has started...")
+		const result = await plex.getWatchLaterVideos();
+		const videos = extract(result.MediaContainer.Video);
+		logVideos(videos);
+		videos.map(downloadVideo)
+		console.log(`=====> All downloads started (${videos.length} download(s) in total) ...`);
+	} catch(e) {
+        throw e;
+	}
+
+	/**
+	 * This is the promise-based syntax version.  Just to appreciate sync/await syntax a little bit more.
+	 
+	return plex.getWatchLaterVideos()
+		.then( res => {
+			console.log("=====> getWatchLaterVideos procedure has started")
+			return res.MediaContainer.Video;
+		})
+		.then(videos => extractVideoUrls(videos))
 		.then(logUrls)
 		.then(urls => urls.map(downloadVideo))
+		.then( _ => console.log("=====> All downloads started..."))
 		.catch((error) => {
 			console.error("The following error was caught:\n");
             throw error;
-        })
+		})
+		
+		*/
 }
 
 module.exports = getWatchLaterVideos;
 
 /************************************** */
 
-const extractVideoUrls = videos => {
+const extract = videos => {
+	if(videos === undefined) throw new Error("There are currently no videos in the Watch Later playlist"); 
 	let videoUrls = new Array();
-	videos.map(vid=>
-		videoUrls.push(vid.url)
-	);
+	//when videos is NOT an array
+	!videos.map && videoUrls.push(extractMetada(videos))
+	//when videos IS an array
+	videos.map && videos.map(video=>videoUrls.push(extractMetada(video)));
 	return videoUrls;
 }
 
-const logUrls = async urlsArr => {
-	const logFilePath = "urls.txt";
-	await deleteFile(logFilePath);
-	const urlsStr = urlsArr.join("\n");
-	console.log(`Videos to download:\n${urlsStr}`);
-	writeFile(logFilePath, urlsStr);
-	return urlsArr;
+const extractMetada = video => {
+	return {
+		title: video.title,
+		url: video.url
+	}
 }
 
-const downloadVideo = videoUrl => {
-	ytdl.getInfo(videoUrl).then(vidInfo => {
+const logVideos = async videos => {
+	const logFilePath = "downloads.txt";
+	await deleteFile(logFilePath);
+	const urlsStr = beautify(JSON.stringify(videos));
+	console.log(`Videos to download:\n${urlsStr}`);
+	await writeFile(logFilePath, urlsStr);
+	return videos;
+}
+
+const downloadVideo = video => {
+	ytdl.getInfo(video.url).then(vidInfo => {
 		const formats = vidInfo.formats.filter(format => {
 			return format.container === "mp4" && format.quality === "medium"
 		})
-		const video = ytdl(videoUrl, {format: formats[0]});
-		video.pipe(fs.createWriteStream(path.resolve('./Documentaries', `${sanitize(vidInfo.title)}.mp4`)));
+		const dl = ytdl(video.url, {format: formats[0]});
+		dl.pipe(fs.createWriteStream(path.resolve('./Documentaries', `${sanitize(vidInfo.title)}.mp4`)));
 	}, err => {
 	  console.error("this is now handled::::::::::>" + err)
 	})	
---
+++ src/getWatchLaterVideos/index.spec.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const plexAPI = require('./index');
+
+describe('', function() {
+
+  /**
+   * Test to implement:  need to test the function 'extractVideoUrls' to ensure that the behaviour adapts when:
+    - no videos are in the WL list
+    - only 1 video in the WL list
+    - 2+ videos in the WL list
+   */
+  describe('', function() {
+    it('test pending implementation');
+  });
+});
