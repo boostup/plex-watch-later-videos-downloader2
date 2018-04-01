406febf17ccca8cdfa8c0b7dff039c34099cac60
Fred B <boostup@gmail.com>
2018-04-01 18:10:37 +0200

    added updateVideosMetadata module

---
+++ src/updateVideosMetadata/index.js
@@ -0,0 +1,42 @@
+const plexApi = require('@boostup/plex-api')
+const ytSearch = require('../utils/yt-api/index')
+
+plexApi.setConfig(require('./config').plex);
+ytSearch.setConfig(require('./config').yt);
+
+const updateAllVideosMetadata = () => {
+    plexApi.getDocus()
+    .then( docus => docus.map(handleDocu) )
+    .then(plexApi.launchFileScan)
+}
+
+module.exports = updateVideosMetadata;
+
+/********************************************* */
+
+
+const handleDocu = (docu) => {
+    const {ratingKey, title, summary} = docu;
+
+    if(summary) 
+        return console.info(`docu ${ratingKey} (${title}) already has a summary;  aborting metadata update`);
+
+    console.info(`Querying youtube with title: ${title}`); 
+
+    ytSearch.searchByTitle(title, 1)
+        .then(getYTVidWithFullDesc)
+        .then(YTVideo => updateMetadata(ratingKey, YTVideo))
+        .catch(err => console.error(err))
+        .then(() => console.log('-------------------'));
+
+}
+
+const getYTVidWithFullDesc = (YTVideo) => {
+    console.log(`Querying Youtube with id: ${YTVideo.id}`);
+    return ytSearch.searchByVideoId(YTVideo.id)  
+}
+
+const updateMetadata = (plexDocuId, YTVideo) => {
+    console.log(`Updating plexDocuId: ${plexDocuId} / Youtube with id:${YTVideo.id}`);
+    plexApi.updateMetadata(plexDocuId, YTVideo);
+}
