ce548e02ee547f33ec94e4100a9184f1ca589ed9
Frédéric Beauvois <boostup@funcarousel-1.home>
2018-04-01 21:42:07 +0200

    trying to have a pause between downloads and metadata updates, but failing at it

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 const getWatchLaterVideos = require('./getWatchLaterVideos/index')
 const updateVideosMetadata = require('./updateVideosMetadata/index')
+const wait = require('./utils/flow/wait');
 
 async function main() {
 	try {
-		await getWatchLaterVideos()
-		await updateVideosMetadata()
+		await getWatchLaterVideos();
+		//This wait stuff is not working!!!
+		wait(10000);
+		await updateVideosMetadata();
 	} catch (error) {
 		console.error(error);
 	}
 
 }
 module.exports = main
+
+/***************************** */
+
