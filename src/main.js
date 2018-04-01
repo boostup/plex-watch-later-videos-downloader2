620844f47235a690b04882b2f212115f349b45be
Fred B <boostup@gmail.com>
2018-04-01 18:10:53 +0200

    using updateVideosMetadata module in main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const getWatchLaterVideos = require('./getWatchLaterVideos/index')
+const updateVideosMetadata = require('./updateVideosMetadata/index')
 
 function main() {
 	getWatchLaterVideos();
+	updateVideosMetadata();
 }
 module.exports = main
