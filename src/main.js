4058203cc5780b87426b9b3465580f3aee29243c
Frédéric Beauvois <boostup@funcarousel-1.home>
2018-04-18 10:49:31 +0200

    corrections for new npm script 'updateMetada'

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ const wait = require('./utils/flow/wait');
 async function main() {
 	try {
 		await getWatchLaterVideos();
-		//This wait stuff is not working!!!
-		wait(10000);
-		await updateVideosMetadata();
+		//This wait stuff is not working so comenting it!!!
+		// wait(10000);
+		// await updateVideosMetadata();
 	} catch (error) {
 		console.error(error);
 	}
